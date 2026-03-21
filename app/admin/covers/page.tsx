1c5675b890658d585c3052fc79e85fbea67395f9
Ariunzul Dolgorsuren <ariunzuldolgorsuren7@gmail.com>
2026-03-21 17:57:38 +0800

    pages create

---
+++ app/admin/covers/page.tsx
@@ -0,0 +1,7 @@
+export default function Covers() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-2xl font-bold">Covers</h1>
+    </div>
+  );
+}
